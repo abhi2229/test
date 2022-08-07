a4420abc8fc8f2ae37f33a7612f309a167a1017b
Abhishek Maiti <maity2229@gmail.com>
2022-08-07 23:51:07 +0530

    Commited

--- ci_cd_based_thyroid_detection-main/app.py
+++ ci_cd_based_thyroid_detection-main/app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/',methods=['GET','POST'])
 def hello_world():
     return render_template ("index.html")
-  #  return 'Hello, World!'
+   #return 'Hello, World!'
 
 if __name__ == '__main__':
-    app.run(debug=True,port=8000)
+    app.run(debug=True)
