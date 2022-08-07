903e4d6818fdb2e9558b903ebadaf3573b874ea9
Abhishek Maiti <maity2229@gmail.com>
2022-08-08 00:15:08 +0530

    Final:

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/',methods=['GET','POST'])
 def hello_world():
-    return render_template ("index.html")
+   return render_template ("index.html")
    #return 'Hello, World!'
 
 if __name__ == '__main__':
